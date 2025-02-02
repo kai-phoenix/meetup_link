9527326eb385d7c6cc7ffc6812d5aebef4a7b0e7
kai-phoenix <zax04097@outlook.jp>
2025-02-02 23:57:38 +0900

    [tmp]認証機能の導入(sanctum)
    認証機能をsanctumに導入し、コントローラーを作成

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // ログイン処理
+    public function login(Request $request)
+    {
+
+    }
+    // ログアウト処理
+    public function logout()
+    {
+
+    }
+}
