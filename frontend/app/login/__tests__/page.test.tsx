e2d9ecb1a6a29c965a9fd6b7abc428453ed5dfa1
kai-phoenix <zax04097@outlook.jp>
2025-02-18 00:24:31 +0900

    [hotfix]ログインテストファイルの修正
    useRouterをマウントするため、テストファイルにモックを記述

--- frontend/app/login/__tests__/page.test.tsx
+++ frontend/app/login/__tests__/page.test.tsx
@@ -1,5 +1,13 @@
 import {render,screen} from '@testing-library/react';
+import '@testing-library/jest-dom/';
 import LoginPage from '../page';
+
+// useRouterをモック化
+jest.mock('next/router',() => ({
+    useRouter:() => ( {
+        push:jest.fn(),
+    })
+}))
 
 describe('LoginPage',() => {
     it('renders a login form',() => {
