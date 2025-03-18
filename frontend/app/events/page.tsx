d8ec339b983b97bb432c2d4db7f861821ea3415f
kai-phoenix <zax04097@outlook.jp>
2025-03-18 18:50:22 +0900

    [tmp]画像投稿機能の追加4
    画像表示に成功4

--- frontend/app/events/page.tsx
+++ frontend/app/events/page.tsx
@@ -48,20 +48,18 @@ export default function EventPage() {
             <div className='mb-6'>
                 <a href='/events/create' className='bg-blue-500 text-white hover:bg-gray-700 rounded-2xl p-2'>新規作成</a>
             </div>
-            <ul className="max-w-screen-sm">
+            <ul>
                 {event.map((event: Event) => (
                     <li key={event.id}>
                         <div className='my-2 flex justify-end'>
                             <EditButton editPath={`/events/${event.id}/edit`} name="編集"/>
                             <RemoveButton removePath = {`/events/${event.id}/cancel`} name="削除"/>
                         </div>
-                        <Image src ={`${process.env.NEXT_PUBLIC_IMAGE_URL}/storage/event_images/sample1.jpg`} width={500} height={500} alt="Event image"/>
-                        <div className='flex align-center border-2 border-cyan-200 mb-8 p-4 min-w-max shadow-md shadow-cyan-500'>
-                            <div className='my-auto'>
-                                <EditButton editPath={`/events/${event.id}/edit`} name="編集"/>
-                                <RemoveButton removePath = {`/events/${event.id}/cancel`} name="削除"/>
+                        <div className='flex align-center border-2 border-cyan-200 mb-8 p-4 min-w-80 shadow-md shadow-cyan-500'>
+                            <div className = 'relative w-full h-auto aspect-square min-w-72 max-w-lg mb-auto'>
+                                <Image src ={`${process.env.NEXT_PUBLIC_IMAGE_URL}/storage/event_images/sample1.jpg`} alt="Event image" fill className='object-cover pr-4'/>
                             </div>
-                            <div className='flex flex-col'>
+                            <div className='flex flex-col justify-between'>
                                 <a href = {`/events/${event.id}/edit`} className='mr-4 my-4 pr-4 py-4 rounded-lg'>
                                     <div className='min-w-72 text-xl'>
                                         <p>No.{event.id}</p>
