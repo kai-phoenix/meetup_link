179706b8f8a8c06f8f3f4fcf05bf8c773dcc857f
kai-phoenix <zax04097@outlook.jp>
2025-03-21 00:55:14 +0900

    [fix]プロフィールページのレイアウト修正
    プロフィールページのレイアウト修正

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import {useState,useEffect} from 'react'
 import { useRouter } from 'next/navigation'
+import { UpdateButton } from '../components/UpdateButton'
+import { ReturnButton } from '../components/ReturnButton'
 import { User } from '@/types/user'
 
 export default function ProfilePage() {
@@ -43,11 +45,16 @@ export default function ProfilePage() {
         return <div>ロード中・・・</div>
     }
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>ID:{user.id}</p>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
+        <div className="w-6/12 mx-auto max-w-lg min-w-96">
+            <h1 className="my-5 text-2xl font-bold">プロフィール</h1>
+            <div className="flex justify-center flex-col my-10 p-6 border-2 border-cyan-200 shadow-md shadow-cyan-500">
+                <p>名前: {user.name}</p>
+                <p>メールアドレス: {user.email}</p>
+            </div>
+            <div className='mt-2.5 mb-2.5 w-11/12 mx-auto flex gap-1'>
+                <UpdateButton name="更新"/>
+                <ReturnButton name="戻る"/>
+            </div>
         </div>
     )
 }
